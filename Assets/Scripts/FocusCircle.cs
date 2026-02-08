3fcf5eb79d6547a2dfc01e1646d9fe19e256bc58
numbersguy104 <numbersboy@icloud.com>
2026-02-07 21:16:36 -0500

    "Follow the circle" base behavior
    The circle moves around randomly. If the player's mouse is not on the circle, it shrinks. When the circle is at 0 size, the game restarts.

---
+++ Assets/Scripts/FocusCircle.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FocusCircle : MonoBehaviour
+{
+    [Tooltip("Speed of the circle's random movement, in units/second")]
+    [SerializeField] private float speed = 100.0f;
+    [Tooltip("Size the circle will lose per second (as a fraction of the total size) when the mouse is not in the circle")]
+    [SerializeField] private float shrinkRate = 0.2f;
+
+    private bool gameStarted = false;
+    private float startingDiameter;
+    private Vector2 destination;
+    private Vector2 screenSize;
+    private RectTransform _rectTransform;
+
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        startingDiameter = _rectTransform.rect.size.x;
+
+        screenSize = transform.parent.GetComponent<RectTransform>().rect.size;
+    }
+    private void Update()
+    {
+        Vector2 position = new Vector2(_rectTransform.position.x, _rectTransform.position.y);
+        Vector2 mousePosition = Input.mousePosition;
+
+        if (gameStarted)
+        {
+            if (position == destination)
+            {
+                NewDestination();
+            }
+
+            if (Vector2.Distance(position, mousePosition) > _rectTransform.rect.size.x / 2)
+            {
+                Debug.Log(position);
+                Debug.Log(mousePosition);
+                float newDiameter = _rectTransform.rect.size.x - (startingDiameter * shrinkRate * Time.deltaTime);
+                _rectTransform.sizeDelta = new Vector2(newDiameter, newDiameter);
+
+                if (_rectTransform.rect.size.x <= 0)
+                {
+                    SceneManager.LoadScene(0);
+                }
+            }
+
+            float tickSpeed = speed * Time.deltaTime;
+            _rectTransform.position = Vector2.MoveTowards(_rectTransform.position, destination, tickSpeed);
+        }
+        else if (Vector2.Distance(position, mousePosition) <= _rectTransform.rect.size.x / 2)
+        {
+            gameStarted = true;
+            NewDestination();
+        }
+
+
+    }
+
+    private void NewDestination()
+    {
+        //Pick a random point on screen, but don't go off the edge of the screen
+        float x = Random.value * (screenSize.x - _rectTransform.rect.size.x / 2);
+        float y = Random.value * (screenSize.y - _rectTransform.rect.size.y / 2);
+        destination = new Vector2(x, y);
+    }
+}
